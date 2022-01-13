2923aee47f4f54a73afec3ca06b4c76bca7ce1d1
Milo Felbapov <mfelbapov@pillartechnology.com>
2022-01-13 13:16:52 -0600

    Add Arc GIS

--- XamarinArcGis/XamarinArcGis/MainPage.xaml.cs
+++ XamarinArcGis/XamarinArcGis/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Esri.ArcGISRuntime.Mapping;
 using Xamarin.Forms;
 
 namespace XamarinArcGis
@@ -13,6 +8,13 @@ namespace XamarinArcGis
         public MainPage()
         {
             InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Map myMap = new Map(Basemap.CreateImagery());
+            MyMapView.Map = myMap;
         }
     }
 }
--- XamarinArcGis/XamarinArcGis/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ XamarinArcGis/XamarinArcGis/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace XamarinArcGis {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Esri.ArcGISRuntime.Xamarin.Forms.MapView MyMapView;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            MyMapView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Esri.ArcGISRuntime.Xamarin.Forms.MapView>(this, "MyMapView");
         }
     }
 }
